b69c66c09e737acf564494995ebfcc57edc81bcd
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-12-04 13:53:05 +0100

    initial commit

---
+++ plugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Reading Time Blocks
+Description: Gutenberg blocks to display a post reading time.
+Version: 0.1.0
+Author: Luca Tumedei
+Author URI: https://theaveragedev.com
+Text Domain: reading-blocks
+*/
